ce005446296d486ba7c97362e67b92da58feb77b
Nikolay Sokolov <sokolov.nicholas@gmail.com>
2021-04-05 09:58:49 +0300

    HW07 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,79 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"github.com/cheggaaa/pb/v3"
+	"io"
+	"math"
+	"os"
 )
 
 var (
-	ErrUnsupportedFile       = errors.New("unsupported file")
+	// ErrUnsupportedFile is happening when file is not supported
+	ErrUnsupportedFile = errors.New("unsupported file")
+	// ErrOffsetExceedsFileSize is happening when offset exceeds the file size
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
+// Copy is analog of regular cp
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
-	return nil
+	srcStat, err := os.Stat(fromPath)
+	if err != nil {
+		return err
+	}
+
+	if offset > srcStat.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if limit == 0 {
+		limit = srcStat.Size() - offset
+	}
+
+	src, err := os.Open(fromPath)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	defer src.Close()
+
+	dst, err := os.Create(toPath)
+	if err != nil {
+		return fmt.Errorf("could not create file, %w", err)
+	}
+	defer dst.Close()
+
+	_, err = src.Seek(offset, 0)
+	if err != nil {
+		return ErrOffsetExceedsFileSize
+	}
+
+	maxBar := limit
+	bar := pb.Full.Start64(maxBar)
+	srcReader := bar.NewProxyReader(src)
+
+	buf := make([]byte, int(math.Min(float64(1024), float64(limit))))
+	for {
+		n, err := srcReader.Read(buf)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				return nil
+			}
+			return err
+		}
+
+		_, err = dst.Write(buf[:n])
+		if err != nil {
+			return err
+		}
+
+		maxBar -= int64(n)
+		if maxBar <= 0 {
+			break
+		}
+	}
+
+	bar.SetCurrent(limit)
+	bar.Finish()
+
+	return err
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,84 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+type TestCase struct {
+	offset   int64
+	limit    int64
+	testFile string
+}
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	cases := []TestCase{
+		{
+			offset:   0,
+			limit:    0,
+			testFile: "out_offset0_limit0.txt",
+		},
+		{
+			offset:   0,
+			limit:    10,
+			testFile: "out_offset0_limit10.txt",
+		},
+		{
+			offset:   0,
+			limit:    1000,
+			testFile: "out_offset0_limit1000.txt",
+		},
+		{
+			offset:   0,
+			limit:    10000,
+			testFile: "out_offset0_limit10000.txt",
+		},
+		{
+			offset:   100,
+			limit:    1000,
+			testFile: "out_offset100_limit1000.txt",
+		},
+		{
+			offset:   6000,
+			limit:    1000,
+			testFile: "out_offset6000_limit1000.txt",
+		},
+	}
+	input := "testdata/input.txt"
+	for _, f := range cases {
+		file, err := ioutil.TempFile("", "")
+		require.NoError(t, err)
+
+		t.Run(f.testFile, func(t *testing.T) {
+			err := Copy(input, file.Name(), f.offset, f.limit)
+			require.NoError(t, err)
+
+			copied, err := ioutil.ReadFile(file.Name())
+			require.NoError(t, err)
+
+			fixture, err := ioutil.ReadFile(fmt.Sprintf("testdata/%s", f.testFile))
+			require.NoError(t, err)
+
+			require.Equal(t, fixture, copied)
+		})
+	}
+
+	t.Run("dev/null", func(t *testing.T) {
+		err := Copy(input, "/dev/null", 0, 0)
+		require.NoError(t, err)
+	})
+}
+
+func TestCopyInvalid(t *testing.T) {
+	t.Run("Offset exceeded the file size", func(t *testing.T) {
+		input := "testdata/input.txt"
+		fileStat, err := os.Stat(input)
+		require.NoError(t, err)
+
+		err = Copy(input, "/dev/null", fileStat.Size()+1, 0)
+		require.ErrorIs(t, err, ErrOffsetExceedsFileSize)
+	})
 }
